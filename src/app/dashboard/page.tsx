6b3ea0e4f466373eec0bcab37a6d559ab7aa728a
unknown <avsanguenza@up.edu.ph>
2024-04-16 03:07:30 +0900

    Created first card prototype for events

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,9 +17,14 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
+//TODO: dialogue for details 
+//    : db setup/fetch from setup
 export default function Page(){
+  
     return (
-        <header className="bg-teal-500">
+      <>
+        <header className="bg-pink-500">
                   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                   <div className="flex lg:flex-1">
                   <div className="flex space-x-4">
@@ -28,7 +33,7 @@ export default function Page(){
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-teal-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-pink-700 text-white' : 'text-gray-300 hover:bg-base hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -41,7 +46,7 @@ export default function Page(){
                   <div className="text-white sm:pr-16">Welcome user!</div>        
                   <button
                   type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="relative rounded-full bg-gray-800 p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
@@ -50,14 +55,13 @@ export default function Page(){
 
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="relative flex rounded-full text-white bg-base text-sm focus:outline-none  focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
                     </Menu.Button>
                   </div>
                   <Transition
@@ -74,7 +78,7 @@ export default function Page(){
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-pink-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
                           </a>
@@ -84,22 +88,24 @@ export default function Page(){
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-pink-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
                           </a>
                         )}
                       </Menu.Item>
-                      <Menu.Item>
+                    <div className="py-1">
+                    <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="/"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            href="#"
+                            className={classNames(active ? 'bg-pink-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Sign out
                           </a>
                         )}
                       </Menu.Item>
+                    </div>
                     </Menu.Items>
                   </Transition>
                 </Menu>
@@ -110,7 +116,33 @@ export default function Page(){
                 </div>
                     </nav>
         </header>
+        
 
+<div class="mt-10 ml-5  max-w-sm p-6 text-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <img className="h-auto max-w-full rounded-lg" src="https://www.peerspace.com/resources/wp-content/uploads/atlanta-Beautiful-Urban-Garden.webp"/>
+
+    <a href="#">
+        <h5 class="mt-4 text-2xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Event Name</h5>
+    </a>
+    <h2 className="mt-2">Event Place</h2>
+    <hr className="h-px my-3 bg-gray-300 border-0 dark:bg-gray-700"></hr>
+
+    <ul className="flex items-center w-full me-4">
+      <li><p class="mt-2  text-left font-normal  dark:text-gray-400">Organizer name here</p></li>
+      <li>
+     <a href="#" className='mt-3 inline-flex items-center'>
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="ml-24 w-8 h-4">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" /> 5
+</svg>
+                        4.0
+     </a>
+</li>
+    </ul>
+    <button type="button" class="mt-4 text-white bg-pink-500 hover:bg-pink-700 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">See more</button>
+
+</div>
+
+</>
     );
 
 }
